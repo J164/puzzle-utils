bd7ad5c1d9e56334128970480337fb62648370f2
Jacob Edley <jac.dan@icloud.com>
2024-04-08 00:18:26 -0500

    implement candidates for sudoku

---
+++ src/puzzles/sudoku/mod.rs
@@ -0,0 +1,54 @@
+const NUM_MIN: u8 = 1;
+const NUM_MAX: u8 = 9;
+
+const GRID_SIZE: usize = 9;
+const BOX_SIZE: usize = 3;
+
+fn solve_sudoku(sudoku: &mut [Option<u8>]) {}
+
+pub fn candidates(sudoku: &[Option<u8>], position: usize) -> Vec<u8> {
+    let row = position / GRID_SIZE;
+    let col = position % GRID_SIZE;
+    let box_start = ((row / 3) * GRID_SIZE + col / 3) * BOX_SIZE;
+
+    (NUM_MIN..=NUM_MAX)
+        .filter(|candidate| {
+            let row_iter = (row * GRID_SIZE)..((row + 1) * GRID_SIZE);
+            let col_iter = (col..=(col + (GRID_SIZE - 1) * (GRID_SIZE - 1))).step_by(GRID_SIZE);
+            let box_iter = (0..BOX_SIZE)
+                .flat_map(|x| (box_start + x * GRID_SIZE)..(box_start + x * GRID_SIZE + BOX_SIZE));
+
+            row_iter
+                .chain(col_iter)
+                .chain(box_iter)
+                .all(|x| sudoku[x] != Some(*candidate))
+        })
+        .collect()
+}
+
+pub fn print_sudoku(sudoku: &[Option<u8>]) {
+    for row in 0..GRID_SIZE {
+        for col in 0..GRID_SIZE {
+            print!(
+                "{}{}",
+                match sudoku[row * 9 + col] {
+                    Some(x) => x.to_string(),
+                    None => " ".to_string(),
+                },
+                if col % GRID_SIZE == GRID_SIZE - 1 {
+                    '\n'
+                } else if col % BOX_SIZE == BOX_SIZE - 1 {
+                    '|'
+                } else {
+                    ' '
+                }
+            );
+        }
+
+        if row % BOX_SIZE == BOX_SIZE - 1 && row % GRID_SIZE != GRID_SIZE - 1 {
+            for col in 0..GRID_SIZE {
+                print!("—{}", if col == GRID_SIZE - 1 { '\n' } else { ' ' });
+            }
+        }
+    }
+}
