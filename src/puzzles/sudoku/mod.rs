dc52f0bdc9fb951a08960e76bd10bca43ef75661
Jacob Edley <jac.dan@icloud.com>
2024-05-08 14:21:11 -0500

    add sudoku tests

--- src/puzzles/sudoku/mod.rs
+++ src/puzzles/sudoku/mod.rs
@@ -11,13 +11,18 @@ pub const GRID_SIZE: usize = 9;
 const BOX_SIZE: usize = 3;
 
 pub fn solve_sudoku(original: Vec<u8>) -> Option<(RgbBuffer, RgbBuffer)> {
+    let solved = solve(&original)?;
+    Some((print_sudoku(original), print_sudoku(solved)))
+}
+
+fn solve(original: &Vec<u8>) -> Option<Vec<u8>> {
     let mut sudoku = original.to_owned();
     let mut stack = Vec::with_capacity(GRID_SIZE * GRID_SIZE);
 
     if let Some(square) = next_blank(&sudoku, 0) {
         stack.push(square);
     } else {
-        return Some((print_sudoku(original), print_sudoku(sudoku)));
+        return Some(sudoku);
     }
 
     while !stack.is_empty() {
@@ -34,7 +39,7 @@ pub fn solve_sudoku(original: Vec<u8>) -> Option<(RgbBuffer, RgbBuffer)> {
         if let Some(square) = next_blank(&sudoku, *index) {
             stack.push(square);
         } else {
-            return Some((print_sudoku(original), print_sudoku(sudoku)));
+            return Some(sudoku);
         }
     }
 
@@ -128,4 +133,111 @@ fn print_sudoku(sudoku: Vec<u8>) -> RgbBuffer {
     }
 
     image
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn easy_solve() {
+        let original = vec![
+            4, 1, 5, 8, 3, 0, 0, 9, 0, 0, 0, 3, 0, 0, 9, 1, 0, 4, 0, 0, 2, 1, 5, 0, 0, 0, 6, 9, 0,
+            0, 7, 8, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 3, 8, 1, 5, 0, 0, 0, 1, 2, 4, 0, 0, 0, 0, 4, 9,
+            0, 0, 0, 6, 3, 3, 8, 0, 5, 0, 0, 0, 4, 0, 0, 0, 9, 3, 0, 7, 5, 0, 0,
+        ];
+        let expected = vec![
+            4, 1, 5, 8, 3, 6, 2, 9, 7, 8, 6, 3, 2, 7, 9, 1, 5, 4, 7, 9, 2, 1, 5, 4, 8, 3, 6, 9, 4,
+            1, 7, 8, 3, 6, 2, 5, 2, 7, 6, 4, 9, 5, 3, 8, 1, 5, 3, 8, 6, 1, 2, 4, 7, 9, 1, 5, 4, 9,
+            2, 8, 7, 6, 3, 3, 8, 7, 5, 6, 1, 9, 4, 2, 6, 2, 9, 3, 4, 7, 5, 1, 8,
+        ];
+
+        let solution = solve(&original);
+
+        assert!(solution.is_some());
+
+        assert_eq!(solution.expect("should be some"), expected);
+    }
+
+    #[test]
+    fn medium_solve() {
+        let original = vec![
+            5, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 9, 0, 0, 0, 0, 2, 7, 4, 0, 0, 1, 0, 5, 0, 0, 9, 2, 0,
+            0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 6, 0, 4, 9, 0, 0, 0, 3, 0, 0, 0,
+            2, 7, 9, 0, 0, 0, 8, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 0, 1, 2,
+        ];
+        let expected = vec![
+            5, 7, 8, 9, 6, 2, 3, 4, 1, 6, 1, 9, 4, 8, 3, 5, 2, 7, 4, 2, 3, 1, 7, 5, 8, 6, 9, 2, 9,
+            4, 3, 1, 8, 6, 7, 5, 8, 3, 7, 2, 5, 6, 1, 9, 4, 1, 5, 6, 7, 4, 9, 2, 3, 8, 3, 4, 1, 5,
+            2, 7, 9, 8, 6, 7, 8, 2, 6, 9, 1, 4, 5, 3, 9, 6, 5, 8, 3, 4, 7, 1, 2,
+        ];
+
+        let solution = solve(&original);
+
+        assert!(solution.is_some());
+
+        assert_eq!(solution.expect("should be some"), expected);
+    }
+
+    #[test]
+    fn hard_solve() {
+        let original = vec![
+            0, 0, 0, 0, 3, 0, 4, 0, 0, 9, 0, 0, 4, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 7, 2, 0, 0,
+            9, 0, 0, 5, 0, 0, 0, 8, 0, 0, 0, 1, 0, 0, 0, 0, 7, 0, 0, 6, 0, 0, 5, 2, 9, 0, 0, 0, 1,
+            0, 0, 7, 0, 0, 6, 0, 1, 0, 5, 0, 0, 0, 8, 0, 4, 0, 0, 0, 0, 0, 1, 0,
+        ];
+        let expected = vec![
+            1, 7, 2, 5, 3, 8, 4, 9, 6, 9, 8, 6, 4, 7, 2, 3, 5, 1, 3, 5, 4, 9, 6, 1, 8, 7, 2, 4, 6,
+            9, 3, 2, 5, 1, 8, 7, 8, 2, 5, 7, 1, 9, 6, 3, 4, 7, 1, 3, 6, 8, 4, 5, 2, 9, 2, 9, 8, 1,
+            4, 3, 7, 6, 5, 6, 3, 1, 2, 5, 7, 9, 4, 8, 5, 4, 7, 8, 9, 6, 2, 1, 3,
+        ];
+
+        let solution = solve(&original);
+
+        assert!(solution.is_some());
+
+        assert_eq!(solution.expect("should be some"), expected);
+    }
+
+    #[test]
+    fn hard_solve2() {
+        let original = vec![
+            8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 7, 0, 0, 9, 0, 2, 0, 0, 0, 5,
+            0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 4, 5, 7, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, 1, 0,
+            0, 0, 0, 6, 8, 0, 0, 8, 5, 0, 0, 0, 1, 0, 0, 9, 0, 0, 0, 0, 4, 0, 0,
+        ];
+
+        let expected = vec![
+            8, 1, 2, 7, 5, 3, 6, 4, 9, 9, 4, 3, 6, 8, 2, 1, 7, 5, 6, 7, 5, 4, 9, 1, 2, 8, 3, 1, 5,
+            4, 2, 3, 7, 8, 9, 6, 3, 6, 9, 8, 4, 5, 7, 2, 1, 2, 8, 7, 1, 6, 9, 5, 3, 4, 5, 2, 1, 9,
+            7, 4, 3, 6, 8, 4, 3, 8, 5, 2, 6, 9, 1, 7, 7, 9, 6, 3, 1, 8, 4, 5, 2,
+        ];
+
+        let solution = solve(&original);
+
+        assert!(solution.is_some());
+
+        assert_eq!(solution.expect("should be some"), expected);
+    }
+
+    #[test]
+    fn hard_solve3() {
+        let original = vec![
+            1, 2, 0, 3, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, 0, 0,
+            4, 2, 0, 0, 5, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 9, 0, 6, 0, 0, 0, 5, 0, 7, 0, 0, 0, 1, 5,
+            0, 0, 2, 0, 0, 0, 0, 0, 0, 9, 0, 0, 6, 0, 0, 0, 0, 0, 0, 7, 0, 0, 8,
+        ];
+
+        let expected = vec![
+            1, 2, 5, 3, 7, 4, 8, 9, 6, 4, 7, 9, 6, 1, 8, 3, 2, 5, 6, 8, 3, 9, 5, 2, 7, 1, 4, 7, 1,
+            4, 2, 6, 9, 5, 8, 3, 5, 3, 2, 7, 8, 1, 6, 4, 9, 9, 6, 8, 4, 3, 5, 1, 7, 2, 8, 9, 1, 5,
+            4, 6, 2, 3, 7, 2, 5, 7, 8, 9, 3, 4, 6, 1, 3, 4, 6, 1, 2, 7, 9, 5, 8,
+        ];
+
+        let solution = solve(&original);
+
+        assert!(solution.is_some());
+
+        assert_eq!(solution.expect("should be some"), expected);
+    }
 }
