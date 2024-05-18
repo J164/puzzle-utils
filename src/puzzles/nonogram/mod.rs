17ea4ae35d39b6f207f4c03ed6e67479e512208d
Jacob Edley <jac.dan@icloud.com>
2024-05-18 18:43:05 -0500

    add narrow solve strategy and tests to nonogram

--- src/puzzles/nonogram/mod.rs
+++ src/puzzles/nonogram/mod.rs
@@ -11,8 +11,6 @@ use crate::{
     util::{RgbBuffer, BLACK_PIXEL, GRAY_PIXEL, ROBOTO_MEDIUM, WHITE_PIXEL},
 };
 
-type Rule = Vec<Vec<usize>>;
-
 #[derive(Debug, Error)]
 pub enum NonogramError {
     #[error("puzzle cannot be empty")]
@@ -31,53 +29,148 @@ pub fn solve_nonogram(col: &str, row: &str) -> Result<SolutionPair, NonogramErro
         return Err(NonogramError::EmptyPuzzle);
     }
 
-    let col = parse_rule(col, height)?;
-    let row = parse_rule(row, width)?;
+    let col = parse(col, height)?;
+    let row = parse(row, width)?;
 
-    let unsolved = print_nonogram(width as u32, height as u32, &row, &col);
+    let unsolved = print(width as u32, height as u32, &row, &col);
 
-    let grid = solve(width, height, col, row)?;
+    let grid = solve(col, row)?;
 
     let solved = print_solution(width as u32, unsolved.clone(), grid);
 
     Ok(SolutionPair::new(unsolved, solved))
 }
 
-fn parse_rule(rule: &str, rule_height: usize) -> Result<Rule, NonogramError> {
+#[derive(Debug)]
+struct Rule {
+    values: Vec<usize>,
+    size: usize,
+}
+
+fn parse(rule: &str, bound: usize) -> Result<Vec<Rule>, NonogramError> {
     rule.split(';')
         .map(|rule| {
-            let rule = rule
+            let mut size = 0;
+
+            let values = rule
                 .split(',')
                 .map(|x| {
-                    x.parse::<usize>()
-                        .or(Err(NonogramError::InvalidRule(x.to_string())))
+                    let value = x
+                        .parse::<usize>()
+                        .or(Err(NonogramError::InvalidRule(x.to_string())))?;
+
+                    size += value;
+
+                    Ok(value)
                 })
                 .collect::<Result<Vec<usize>, NonogramError>>()?;
 
-            if rule.is_empty() || rule.iter().sum::<usize>() + (rule.len() - 1) > rule_height {
+            size += values.len() - 1;
+
+            if values.is_empty() || size > bound {
                 return Err(NonogramError::InvalidRuleDimension);
             }
 
-            Ok(rule)
+            Ok(Rule { values, size })
         })
-        .collect::<Result<Rule, NonogramError>>()
+        .collect::<Result<Vec<Rule>, NonogramError>>()
 }
 
-fn solve(width: usize, height: usize, col: Rule, row: Rule) -> Result<Vec<bool>, NonogramError> {
-    let mut grid = vec![false; width * height];
+#[derive(Debug, Clone)]
+enum Square {
+    Blank,
+    Filled,
+    Blocked,
+}
+
+fn solve(col: Vec<Rule>, row: Vec<Rule>) -> Result<Vec<Square>, NonogramError> {
+    let width = col.len();
+    let height = row.len();
+
+    let mut grid = vec![Square::Blank; width * height];
+
     // TODO: solve nonogram
+    /* IDEA:
+        Each square on grid has three states (blank, filled, blocked)
+        Grid is WIDTH x HEIGHT
+
+        Narrowing:
+
+        1. Define the variance of a rule as the bound of a rule minus its size
+        2. For any value in a rule that is greater than the variance of that rule there are (value - variance) guaranteed squares
+        3. Skip variance squares and fill guaranteed squares
+
+        Recursive backtrack:
+
+        A is the rule set with the least rules (col by default)
+        B is the other rule set
+
+        1. Iterate through max heap of A rules by size (index -> i, rule)
+        2. IF no fulfilments exist, revert to previous state
+        3. Iterate through fulfilments of rule that replace all blanks with filled or blocked // Because of row rule fulfilments, the effective board height is B.SIZE - i
+            a. Attempt to solve all row rules of size <= (WIDTH - (i + 1)) // should have exactly one solution if puzzle is valid so far
+            b. IF no valid solution exists for a rule, break back to (3)
+            c. ELSE accept fulfilment and continue
+    */
+
+    narrow(&mut grid, &col, &row);
+    recursive_backtrack(&mut grid, &col, &row);
+
     Ok(grid)
 }
 
-fn print_nonogram(width: u32, height: u32, row: &Rule, col: &Rule) -> RgbBuffer {
+fn narrow(grid: &mut [Square], col: &[Rule], row: &[Rule]) {
+    let width = col.len();
+    let height = row.len();
+
+    for (x, rule) in col.iter().enumerate() {
+        let variance = height - rule.size;
+
+        if variance > height / 2 {
+            continue;
+        }
+
+        let mut y = 0;
+        for &value in &rule.values {
+            if value > variance {
+                for k in variance..value {
+                    grid[(y + k) * width + x] = Square::Filled;
+                }
+            }
+            y += value + 1;
+        }
+    }
+
+    for (y, rule) in row.iter().enumerate() {
+        let variance = width - rule.size;
+
+        if variance > width / 2 {
+            continue;
+        }
+
+        let mut x = 0;
+        for &value in &rule.values {
+            if value > variance {
+                for k in variance..value {
+                    grid[y * width + x + k] = Square::Filled;
+                }
+            }
+            x += value + 1;
+        }
+    }
+}
+
+fn recursive_backtrack(grid: &mut [Square], col: &[Rule], row: &[Rule]) {}
+
+fn print(width: u32, height: u32, row: &[Rule], col: &[Rule]) -> RgbBuffer {
     let mut image = ImageBuffer::from_pixel(width * 50 + 150, height * 50 + 150, WHITE_PIXEL);
 
     let font = FontRef::try_from_slice(ROBOTO_MEDIUM).expect("Font should be valid");
 
-    for (y, rule_set) in row.iter().enumerate() {
+    for (y, rule) in row.iter().enumerate() {
         let y = (y as u32) * 50 + 160;
 
-        for (x, rule) in rule_set.iter().enumerate() {
+        for (x, value) in rule.values.iter().enumerate() {
             let x = (x as u32) * 30 + 10;
 
             draw_text_mut(
@@ -87,15 +180,15 @@ fn print_nonogram(width: u32, height: u32, row: &Rule, col: &Rule) -> RgbBuffer 
                 y as i32,
                 30.0,
                 &font,
-                &rule.to_string(),
+                &value.to_string(),
             );
         }
     }
 
-    for (x, rule_set) in col.iter().enumerate() {
+    for (x, rule) in col.iter().enumerate() {
         let x = (x as u32) * 50 + 165;
 
-        for (y, rule) in rule_set.iter().enumerate() {
+        for (y, rule) in rule.values.iter().enumerate() {
             let y = (y as u32) * 30 + 10;
 
             draw_text_mut(
@@ -125,9 +218,9 @@ fn print_nonogram(width: u32, height: u32, row: &Rule, col: &Rule) -> RgbBuffer 
     image
 }
 
-fn print_solution(width: u32, mut image: RgbBuffer, grid: Vec<bool>) -> RgbBuffer {
+fn print_solution(width: u32, mut image: RgbBuffer, grid: Vec<Square>) -> RgbBuffer {
     for (i, square) in grid.iter().enumerate() {
-        if !square {
+        if !matches!(square, Square::Filled) {
             continue;
         }
 
@@ -142,4 +235,305 @@ fn print_solution(width: u32, mut image: RgbBuffer, grid: Vec<bool>) -> RgbBuffe
     }
 
     image
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Square;
+
+    impl PartialEq for super::Rule {
+        fn eq(&self, other: &Self) -> bool {
+            self.values == other.values && self.size == other.size
+        }
+    }
+
+    impl PartialEq for super::Square {
+        fn eq(&self, other: &Self) -> bool {
+            matches!(
+                (self, other),
+                (Self::Blank, Self::Blank)
+                    | (Self::Blocked, Self::Blocked)
+                    | (Self::Filled, Self::Filled)
+            )
+        }
+    }
+
+    // two x two
+    const TWO_TWO_WIDTH: usize = 2;
+    const TWO_TWO_HEIGHT: usize = 2;
+
+    const TWO_TWO_COL_STRING: &str = "2;1";
+    fn two_two_col_expected() -> Vec<super::Rule> {
+        vec![
+            super::Rule {
+                values: vec![2],
+                size: 2,
+            },
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+        ]
+    }
+
+    const TWO_TWO_ROW_STRING: &str = "2;1";
+    fn two_two_row_expected() -> Vec<super::Rule> {
+        vec![
+            super::Rule {
+                values: vec![2],
+                size: 2,
+            },
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+        ]
+    }
+
+    fn two_two_narrowed_expected() -> Vec<Square> {
+        vec![
+            Square::Filled,
+            Square::Filled,
+            Square::Filled,
+            Square::Blank,
+        ]
+    }
+
+    fn two_two_backtracked_expected() -> Vec<Square> {
+        vec![
+            Square::Filled,
+            Square::Filled,
+            Square::Filled,
+            Square::Blocked,
+        ]
+    }
+
+    // two x three
+    const TWO_THREE_WIDTH: usize = 2;
+    const TWO_THREE_HEIGHT: usize = 3;
+
+    const TWO_THREE_COL_STRING: &str = "1,1;2";
+    fn two_three_col_expected() -> Vec<super::Rule> {
+        vec![
+            super::Rule {
+                values: vec![1, 1],
+                size: 3,
+            },
+            super::Rule {
+                values: vec![2],
+                size: 2,
+            },
+        ]
+    }
+
+    const TWO_THREE_ROW_STRING: &str = "1;1;2";
+    fn two_three_row_expected() -> Vec<super::Rule> {
+        vec![
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+            super::Rule {
+                values: vec![2],
+                size: 2,
+            },
+        ]
+    }
+
+    fn two_three_narrowed_expected() -> Vec<Square> {
+        vec![
+            Square::Filled,
+            Square::Blank,
+            Square::Blank,
+            Square::Filled,
+            Square::Filled,
+            Square::Filled,
+        ]
+    }
+
+    fn two_three_backtracked_expected() -> Vec<Square> {
+        vec![
+            Square::Filled,
+            Square::Blocked,
+            Square::Blocked,
+            Square::Filled,
+            Square::Filled,
+            Square::Filled,
+        ]
+    }
+
+    // three x three
+    const THREE_THREE_WIDTH: usize = 3;
+    const THREE_THREE_HEIGHT: usize = 3;
+
+    const THREE_THREE_COL_STRING: &str = "2;1;1";
+    fn three_three_col_expected() -> Vec<super::Rule> {
+        vec![
+            super::Rule {
+                values: vec![2],
+                size: 2,
+            },
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+        ]
+    }
+
+    const THREE_THREE_ROW_STRING: &str = "1;1;2";
+    fn three_three_row_expected() -> Vec<super::Rule> {
+        vec![
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+            super::Rule {
+                values: vec![1],
+                size: 1,
+            },
+            super::Rule {
+                values: vec![2],
+                size: 2,
+            },
+        ]
+    }
+
+    fn three_three_narrowed_expected() -> Vec<Square> {
+        vec![
+            Square::Blank,
+            Square::Blank,
+            Square::Blank,
+            Square::Filled,
+            Square::Blank,
+            Square::Blank,
+            Square::Blank,
+            Square::Filled,
+            Square::Blank,
+        ]
+    }
+
+    fn three_three_backtracked_expected() -> Vec<Square> {
+        vec![
+            Square::Blocked,
+            Square::Blocked,
+            Square::Filled,
+            Square::Filled,
+            Square::Blocked,
+            Square::Blocked,
+            Square::Filled,
+            Square::Filled,
+            Square::Blocked,
+        ]
+    }
+
+    fn test_parse(string: &str, expected: Vec<super::Rule>, bound: usize) {
+        let actual = super::parse(string, bound).expect("should be ok");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn parse() {
+        // two x two
+        test_parse(TWO_TWO_COL_STRING, two_two_col_expected(), TWO_TWO_HEIGHT);
+        test_parse(TWO_TWO_ROW_STRING, two_two_row_expected(), TWO_TWO_WIDTH);
+
+        // two x three
+        test_parse(
+            TWO_THREE_COL_STRING,
+            two_three_col_expected(),
+            TWO_THREE_HEIGHT,
+        );
+        test_parse(
+            TWO_THREE_ROW_STRING,
+            two_three_row_expected(),
+            TWO_THREE_WIDTH,
+        );
+
+        // three x three
+        test_parse(
+            THREE_THREE_COL_STRING,
+            three_three_col_expected(),
+            THREE_THREE_HEIGHT,
+        );
+        test_parse(
+            THREE_THREE_ROW_STRING,
+            three_three_row_expected(),
+            THREE_THREE_WIDTH,
+        );
+    }
+
+    fn test_narrow(col: Vec<super::Rule>, row: Vec<super::Rule>, expected: Vec<super::Square>) {
+        let mut actual = vec![Square::Blank; col.len() * row.len()];
+        super::narrow(&mut actual, &col, &row);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn narrow() {
+        // two x two
+        test_narrow(
+            two_two_col_expected(),
+            two_two_row_expected(),
+            two_two_narrowed_expected(),
+        );
+
+        // two x three
+        test_narrow(
+            two_three_col_expected(),
+            two_three_row_expected(),
+            two_three_narrowed_expected(),
+        );
+
+        // three x three
+        test_narrow(
+            three_three_col_expected(),
+            three_three_row_expected(),
+            three_three_narrowed_expected(),
+        );
+    }
+
+    fn test_backtrack(
+        mut actual: Vec<Square>,
+        col: Vec<super::Rule>,
+        row: Vec<super::Rule>,
+        expected: Vec<super::Square>,
+    ) {
+        super::recursive_backtrack(&mut actual, &col, &row);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn recursive_backtrack() {
+        // two x two
+        test_backtrack(
+            two_two_narrowed_expected(),
+            two_two_col_expected(),
+            two_two_row_expected(),
+            two_two_backtracked_expected(),
+        );
+
+        // two x three
+        test_backtrack(
+            two_three_narrowed_expected(),
+            two_three_col_expected(),
+            two_three_row_expected(),
+            two_three_backtracked_expected(),
+        );
+
+        // three x three
+        test_backtrack(
+            three_three_narrowed_expected(),
+            three_three_col_expected(),
+            three_three_row_expected(),
+            three_three_backtracked_expected(),
+        );
+    }
 }
